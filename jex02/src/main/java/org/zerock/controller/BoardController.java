d24f8c48272f76d02b9e7f59659a1ba328cf6c09
smkim <smkim@DESKTOP-JVJ89LT>
2022-03-27 09:30:34 +0900

    총건수 추가

--- jex02/src/main/java/org/zerock/controller/BoardController.java
+++ jex02/src/main/java/org/zerock/controller/BoardController.java
@@ -33,7 +33,13 @@ public class BoardController {
 	public void list(Criteria cri, Model model) {
 		log.info("list: "+ cri);
 		model.addAttribute("list", service.getList(cri));
-		model.addAttribute("pageMaker", new PageDTO(cri,123));
+		//model.addAttribute("pageMaker", new PageDTO(cri,123));
+		
+		int total = service.getTotal(cri);
+		
+		log.info("total: "+total);
+		
+		model.addAttribute("pageMaker", new PageDTO(cri, total));
 	}
 	
 	@PostMapping("/register")
@@ -109,4 +115,6 @@ public class BoardController {
 		
 		return "redirect:/board/list";
 	}
+	
+	
 }
