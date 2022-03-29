3a5f45cb7dc0f6b0bf425b3ebb83f84f099d97b7
smkim <smkim@DESKTOP-JVJ89LT>
2022-03-29 12:06:34 +0900

    add service

--- jex03/src/main/java/org/zerock/mapper/ReplyMapper.java
+++ jex03/src/main/java/org/zerock/mapper/ReplyMapper.java
@@ -1,5 +1,13 @@
 package org.zerock.mapper;
 
-public interface ReplyMapper {
+import org.zerock.domain.ReplyVO;
 
+public interface ReplyMapper {
+	public int insert(ReplyVO vo);
+	
+	public ReplyVO read(Long rno); //특정 댓글 읽기
+	
+	public int delete (Long rno);
+	
+	public int update(ReplyVO reply);
 }
