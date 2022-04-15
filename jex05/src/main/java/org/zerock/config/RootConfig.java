e7cda1d6c26f8fbcef22652568eee5dba0f6186b
smkim <smkim@DESKTOP-JVJ89LT>
2022-04-15 13:24:34 +0900

    transactional 추가

--- jex05/src/main/java/org/zerock/config/RootConfig.java
+++ jex05/src/main/java/org/zerock/config/RootConfig.java
@@ -8,14 +8,19 @@ import org.mybatis.spring.annotation.MapperScan;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.ComponentScan;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.context.annotation.EnableAspectJAutoProxy;
 import org.springframework.jdbc.datasource.DataSourceTransactionManager;
+import org.springframework.transaction.annotation.EnableTransactionManagement;
 
 import com.zaxxer.hikari.HikariConfig;
 import com.zaxxer.hikari.HikariDataSource;
 
 @Configuration
 @ComponentScan(basePackages = "org.zerock.service")
+@ComponentScan(basePackages = "org.zerock.aop")
 @MapperScan(basePackages = {"org.zerock.mapper"})
+@EnableAspectJAutoProxy
+@EnableTransactionManagement
 public class RootConfig {
 	
 	@Bean
